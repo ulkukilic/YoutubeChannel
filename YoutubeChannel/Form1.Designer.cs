d3f25e65f814c7496e3539df3c7f6ac52b2ced4e
ulkuk <ulkuk@ELF>
2025-02-15 12:33:41 +0100

    Add project files.

---
+++ YoutubeChannel/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace YoutubeChannel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_link = new System.Windows.Forms.TextBox();
+            this.txt_category = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(738, 93);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(573, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(227, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube Channel";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_link);
+            this.groupBox1.Controls.Add(this.txt_category);
+            this.groupBox1.Controls.Add(this.txt_name);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(41, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 317);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "NewVideo";
+            // 
+            // txt_link
+            // 
+            this.txt_link.Location = new System.Drawing.Point(115, 158);
+            this.txt_link.Name = "txt_link";
+            this.txt_link.Size = new System.Drawing.Size(100, 22);
+            this.txt_link.TabIndex = 7;
+            // 
+            // txt_category
+            // 
+            this.txt_category.Location = new System.Drawing.Point(115, 108);
+            this.txt_category.Name = "txt_category";
+            this.txt_category.Size = new System.Drawing.Size(100, 22);
+            this.txt_category.TabIndex = 6;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(115, 58);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 22);
+            this.txt_name.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(19, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 47);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(133, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(28, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Link";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Video Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(293, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(219, 317);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Watch List";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(213, 296);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.webBrowser1);
+            this.groupBox3.Location = new System.Drawing.Point(531, 121);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(219, 317);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Screen";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 18);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(213, 296);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txt_link;
+        private System.Windows.Forms.TextBox txt_category;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ YoutubeChannel/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace YoutubeChannel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // SQL bağlantısı oluşturuluyor
+        SqlConnection baglanti = new SqlConnection("Data Source=elıf;Initial Catalog=UdemyYoutubeChannel;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Bağlantı açılıyor
+                baglanti.Open();
+                // SQL komutu hazırlanıyor
+                SqlCommand komut = new SqlCommand("INSERT INTO TBLYOUTUBE (AD, KATEGORI, LINK) VALUES (@P1, @P2, @P3)", baglanti);
+                // Parametreler ekleniyor
+                komut.Parameters.AddWithValue("@P1", txt_name.Text);
+                komut.Parameters.AddWithValue("@P2", txt_category.Text);
+                komut.Parameters.AddWithValue("@P3", txt_link.Text);
+                // Komut çalıştırılıyor
+                komut.ExecuteNonQuery();
+                // Bağlantı kapatılıyor
+                baglanti.Close();
+                // Başarılı mesajı gösteriliyor
+                MessageBox.Show("Successfully added.");
+                // TextBox'lar temizleniyor
+                txt_name.Clear();
+                txt_category.Clear();
+                txt_link.Clear();
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajı gösteriliyor
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        void video()
+        {
+            // Tabloyu çekmek için SQL komutu
+            SqlDataAdapter data = new SqlDataAdapter("SELECT * FROM TBLYOUTUBE", baglanti);
+            DataTable dataTable = new DataTable();
+            // DataTable'a veri dolduruluyor
+            data.Fill(dataTable);
+            // Veriler DataGridView'a yansıtılıyor
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form yüklendiğinde veriler gösteriliyor
+            video();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                // Seçilen satırdaki hücreye erişim sağlanıyor
+                int choose = e.RowIndex;  // Satır indexi alınıyor
+                if (choose >= 0)  // Satır geçerli mi kontrol ediliyor
+                {
+                    // 'LINK' sütununa karşılık gelen indeks (5) doğru mu?
+                    string link = dataGridView1.Rows[choose].Cells[5].Value.ToString();
+
+                    // Link geçerli mi kontrol edelim
+                    if (Uri.IsWellFormedUriString(link, UriKind.Absolute))
+                    {
+                        // WebBrowser'a link yönlendiriliyor
+                        webBrowser1.Navigate(link);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid URL.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda mesaj gösteriliyor
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            // Seçilen satırdaki veriye ulaşma
+            int choose = dataGridView1.SelectedCells[0].RowIndex;
+            // Link değeri alınıyor
+            string link = dataGridView1.Rows[choose].Cells[5].Value.ToString();
+            // WebBrowser'a link yönlendirmesi yapılıyor
+            webBrowser1.Navigate(link);
+
+        }
+    }
+}
